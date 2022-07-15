1a313549ca72da147b4209b843e127f2fb5ebec0
Christian Factoriza <cfactoriza@gmail.com>
2022-07-15 12:55:30 -0500

    Added Name, Location, Active parameters

--- db/seed.js
+++ db/seed.js
@@ -9,9 +9,9 @@ async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-    const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
+    const albert = await createUser({ username: 'albert', password: 'bertie99', name: "name", location: "location" });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name: "name", location: "location" });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name: "name", location: "location" });
 
     console.log(albert,sandra,glamgal);
 
@@ -34,7 +34,7 @@ async function rebuildDB() {
       throw error;
     }
   }
-
+// Possible call on getAllUsers function is required
 
 // this function should call a query which drops all tables from our database
 async function dropTables() {
@@ -61,7 +61,10 @@ async function createTables() {
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
             );
             `);
             
