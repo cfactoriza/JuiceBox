bee26fdc4d01ff7251258807eb28f2cc9fd2d299
Thomas <thomas-g-smith@hotmail.com>
2022-07-15 11:06:32 -0500

    working on async functions

--- db/index.js
+++ db/index.js
@@ -13,8 +13,10 @@ async function getAllUsers() {
   async function createUser({ username, password }) {
     try {
       const result = await client.query(`
-      INSERT INTO users(username, password)
-      VALUES ($1, $2);
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
       `,[username,password]);
   
       return result
--- db/seed.js
+++ db/seed.js
@@ -10,6 +10,7 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
+    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
 
     console.log(albert);
 
@@ -20,77 +21,69 @@ async function createInitialUsers() {
   }
 }
 
-
 // then modify rebuildDB to call our new function
 async function rebuildDB() {
-  try {
-    client.connect();
-
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-  } catch (error) {
-    throw error;
-  }
-}
-  // this function should call a query which drops all tables from our database
-  async function dropTables() {
-    try {
-      console.log("Starting to drop tables...");
-  
-      await client.query(`
-        DROP TABLE IF EXISTS users;
-      `);
-  
-      console.log("Finished dropping tables!");
-    } catch (error) {
-      console.error("Error dropping tables!");
-      throw error;// we pass the error up to the function that calls dropTables
-    }
-  }
-  
-  // this function should call a query which creates all tables for our database 
-  async function createTables() {
-    try {
-      console.log("Starting to build tables...");
-  
-      await client.query(`
-        CREATE TABLE users (
-          id SERIAL PRIMARY KEY,
-          username varchar(255) UNIQUE NOT NULL,
-          password varchar(255) NOT NULL
-        );
-      `);
-  
-      console.log("Finished building tables!");
-    } catch (error) {
-      console.error("Error building tables!");
-      throw error; // we pass the error up to the function that calls createTables
-    }
-  }
-  
-  async function rebuildDB() {
     try {
       client.connect();
   
       await dropTables();
       await createTables();
+      await createInitialUsers();
     } catch (error) {
-      throw error;
-    }
-  }
-  
-  async function testDB() {
-    try {
-      console.log("Starting to test database...");
-  
-      const users = await getAllUsers();
-      console.log("getAllUsers:", users);
-  
-      console.log("Finished database tests!");
-    } catch (error) {
-      console.error("Error testing database!");
       throw error;
     }
   }
 
+  
+// this function should call a query which drops all tables from our database
+async function dropTables() {
+    try {
+        console.log("Starting to drop tables...");
+        
+        await client.query(`
+        DROP TABLE IF EXISTS users;
+        `);
+        
+        console.log("Finished dropping tables!");
+    } catch (error) {
+        console.error("Error dropping tables!");
+        throw error;// we pass the error up to the function that calls dropTables
+    }
+}
+
+// this function should call a query which creates all tables for our database 
+async function createTables() {
+    try {
+        console.log("Starting to build tables...");
+        
+        await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username varchar(255) UNIQUE NOT NULL,
+            password varchar(255) NOT NULL
+            );
+            `);
+            
+            console.log("Finished building tables!");
+        } catch (error) {
+            console.error("Error building tables!");
+            throw error; // we pass the error up to the function that calls createTables
+        }
+    }
+    
+    
+    async function testDB() {
+        try {
+            console.log("Starting to test database...");
+            
+            const users = await getAllUsers();
+            console.log("getAllUsers:", users);
+            
+            console.log("Finished database tests!");
+        } catch (error) {
+            console.error("Error testing database!");
+            throw error;
+        }
+    }
+    
+    rebuildDB()
