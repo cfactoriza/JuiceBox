2c6e9710476aedb1a95460440b39fd0e6241eadb
Thomas <thomas-g-smith@hotmail.com>
2022-07-15 12:32:53 -0500

    Cleaned up createUsers and createInitialUsers

--- db/seed.js
+++ db/seed.js
@@ -10,9 +10,10 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
 
-    console.log(albert);
+    console.log(albert,sandra,glamgal);
 
     console.log("Finished creating users!");
   } catch(error) {
@@ -34,7 +35,7 @@ async function rebuildDB() {
     }
   }
 
-  
+
 // this function should call a query which drops all tables from our database
 async function dropTables() {
     try {
