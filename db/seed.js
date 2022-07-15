6cf57cabf31ba79cd736f830759fff831a519a00
Thomas <thomas-g-smith@hotmail.com>
2022-07-15 10:31:38 -0500

    built async functions for db

--- db/seed.js
+++ db/seed.js
@@ -1,15 +1,96 @@
-const { client } = require('./index');
+const {
+    client,
+    getAllUsers,
+    createUser,
+} = require('./index');
 
-async function testDB() {
-    try { 
-        client.connect();
-        const result = await client.query(`SELECT * FROM users;`);
-        console.log(result);
-    } catch (error) {
-    console.error(error);
-    } finally {
-    client.end();
-    }
+// new function, should attempt to create a few users
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    const albert = await createUser({ username: 'albert', password: 'bertie99' });
+
+    console.log(albert);
+
+    console.log("Finished creating users!");
+  } catch(error) {
+    console.error("Error creating users!");
+    throw error;
+  }
 }
 
-testDB();
+
+// then modify rebuildDB to call our new function
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+  } catch (error) {
+    throw error;
+  }
+}
+  // this function should call a query which drops all tables from our database
+  async function dropTables() {
+    try {
+      console.log("Starting to drop tables...");
+  
+      await client.query(`
+        DROP TABLE IF EXISTS users;
+      `);
+  
+      console.log("Finished dropping tables!");
+    } catch (error) {
+      console.error("Error dropping tables!");
+      throw error;// we pass the error up to the function that calls dropTables
+    }
+  }
+  
+  // this function should call a query which creates all tables for our database 
+  async function createTables() {
+    try {
+      console.log("Starting to build tables...");
+  
+      await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+      `);
+  
+      console.log("Finished building tables!");
+    } catch (error) {
+      console.error("Error building tables!");
+      throw error; // we pass the error up to the function that calls createTables
+    }
+  }
+  
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+  async function testDB() {
+    try {
+      console.log("Starting to test database...");
+  
+      const users = await getAllUsers();
+      console.log("getAllUsers:", users);
+  
+      console.log("Finished database tests!");
+    } catch (error) {
+      console.error("Error testing database!");
+      throw error;
+    }
+  }
+
